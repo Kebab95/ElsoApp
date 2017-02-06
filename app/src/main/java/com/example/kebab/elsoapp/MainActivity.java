3b8f3cb58ae1588c2488d7d2b722f3b2062a3e0e
Kebab95 <kebalazs95@gmail.com>
2017-02-06 08:49:07 +0100

    test

--- app/src/main/java/com/example/kebab/elsoapp/MainActivity.java
+++ app/src/main/java/com/example/kebab/elsoapp/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.kebab.elsoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
