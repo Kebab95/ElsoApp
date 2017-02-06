01c7fb2a82cfc72b7bf38fcbbb3bc6e4d0612246
Kebab95 <kebalazs95@gmail.com>
2017-02-06 09:18:07 +0100

    Menu Items Ver 1

--- app/src/main/java/com/example/kebab/elsoapp/MainActivity.java
+++ app/src/main/java/com/example/kebab/elsoapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.kebab.elsoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.io.Console;
 
@@ -13,4 +17,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.new_game){
+            Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+        }
+        else if (id==R.id.fuck){
+            Toast.makeText(MainActivity.this,"Bazdmeg",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.valami, menu);
+        return true;
+    }
+
 }
